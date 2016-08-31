5acd6f7d0a37087602579c5419fa61183e3a2932
jonathan-git <jonathan-git@users.noreply.github.com>
2016-08-30 23:28:06 -0500

    Moved Timer class to Utilities namespace.

--- Utilities/Timer.cs
+++ Utilities/Timer.cs
@@ -23,7 +23,7 @@ using System;
 using System.Threading;
 using Prism.Native;
 
-namespace Prism.Windows
+namespace Prism.Windows.Utilities
 {
     /// <summary>
     /// Represents a Windows implementation of an <see cref="INativeTimer"/>.
