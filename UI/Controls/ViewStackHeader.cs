7cb74bfaafe690868917eca70e928735072db999
jonathan-git <jonathan-git@users.noreply.github.com>
2016-10-28 00:22:01 -0500

    Implemented Visual APIs for ViewStackHeader class.

--- UI/Controls/ViewStackHeader.cs
+++ UI/Controls/ViewStackHeader.cs
@@ -21,8 +21,10 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 using System;
 using Prism.Native;
+using Prism.UI;
 using Prism.UI.Media;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
 
 namespace Prism.Windows.UI.Controls
 {
@@ -32,9 +34,56 @@ namespace Prism.Windows.UI.Controls
     public sealed class ViewStackHeader : Grid, INativeViewStackHeader
     {
         /// <summary>
+        /// Occurs when this instance has been attached to the visual tree and is ready to be rendered.
+        /// </summary>
+        public new event EventHandler Loaded;
+
+        /// <summary>
         /// Occurs when the value of a property is changed.
         /// </summary>
         public event EventHandler<FrameworkPropertyChangedEventArgs> PropertyChanged;
+
+        /// <summary>
+        /// Occurs when this instance has been detached from the visual tree.
+        /// </summary>
+        public new event EventHandler Unloaded;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether animations are enabled for this instance.
+        /// </summary>
+        public bool AreAnimationsEnabled
+        {
+            get { return areAnimationsEnabled; }
+            set
+            {
+                if (value != areAnimationsEnabled)
+                {
+                    areAnimationsEnabled = value;
+                    if (areAnimationsEnabled)
+                    {
+                        Transitions = transitions;
+                        ChildrenTransitions = childrenTransitions;
+                    }
+                    else
+                    {
+                        transitions = Transitions;
+                        childrenTransitions = ChildrenTransitions;
+
+                        Transitions = null;
+                        ChildrenTransitions = null;
+                    }
+
+                    OnPropertyChanged(Visual.AreAnimationsEnabledProperty);
+                }
+            }
+        }
+        private bool areAnimationsEnabled = true;
+        private TransitionCollection transitions, childrenTransitions;
+
+        /// <summary>
+        /// Gets or sets the method to invoke when this instance requests an arrangement of its children.
+        /// </summary>
+        public ArrangeRequestHandler ArrangeRequest { get; set; }
 
         /// <summary>
         /// Gets or sets the background for the header.
@@ -127,6 +176,85 @@ namespace Prism.Windows.UI.Controls
         private Brush foreground;
 
         /// <summary>
+        /// Gets or sets a <see cref="Rectangle"/> that represents
+        /// the size and position of the object relative to its parent container.
+        /// </summary>
+        public Rectangle Frame
+        {
+            get { return frame; }
+            set
+            {
+                frame = value;
+
+                Width = value.Width;
+                Height = value.Height;
+            }
+        }
+        private Rectangle frame = new Rectangle();
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance can be considered a valid result for hit testing.
+        /// </summary>
+        public new bool IsHitTestVisible
+        {
+            get { return base.IsHitTestVisible; }
+            set
+            {
+                if (value != base.IsHitTestVisible)
+                {
+                    base.IsHitTestVisible = value;
+                    OnPropertyChanged(Visual.IsHitTestVisibleProperty);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the header is inset on top of the view stack content.
+        /// A value of <c>false</c> indicates that the header offsets the view stack content.
+        /// </summary>
+        public bool IsInset
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance has been loaded and is ready for rendering.
+        /// </summary>
+        public bool IsLoaded { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the method to invoke when this instance requests a measurement of itself and its children.
+        /// </summary>
+        public MeasureRequestHandler MeasureRequest { get; set; }
+
+        /// <summary>
+        /// Gets or sets transformation information that affects the rendering position of this instance.
+        /// </summary>
+        public new INativeTransform RenderTransform
+        {
+            get { return renderTransform; }
+            set
+            {
+                if (value != renderTransform)
+                {
+                    renderTransform = value;
+                    base.RenderTransform = renderTransform as Media.Transform ?? renderTransform as global::Windows.UI.Xaml.Media.Transform;
+                    OnPropertyChanged(Visual.RenderTransformProperty);
+                }
+            }
+        }
+        private INativeTransform renderTransform;
+
+        /// <summary>
+        /// Gets or sets the visual theme that should be used by this instance.
+        /// </summary>
+        public new Theme RequestedTheme
+        {
+            get { return base.RequestedTheme.GetTheme(); }
+            set { base.RequestedTheme = value.GetElementTheme(); }
+        }
+
+        /// <summary>
         /// Gets or sets the title for the header.
         /// </summary>
         public string Title
@@ -159,6 +287,61 @@ namespace Prism.Windows.UI.Controls
             BorderThickness = new global::Windows.UI.Xaml.Thickness(0, 0, 0, 1);
             Height = 64;
             HorizontalAlignment = global::Windows.UI.Xaml.HorizontalAlignment.Stretch;
+
+            base.Loaded += (o, e) =>
+            {
+                if (!IsLoaded)
+                {
+                    IsLoaded = true;
+                    OnPropertyChanged(Visual.IsLoadedProperty);
+                    Loaded(this, EventArgs.Empty);
+                }
+            };
+
+            base.Unloaded += (o, e) =>
+            {
+                if (IsLoaded)
+                {
+                    IsLoaded = false;
+                    OnPropertyChanged(Visual.IsLoadedProperty);
+                    Unloaded(this, EventArgs.Empty);
+                }
+            };
+        }
+
+        /// <summary>
+        /// Measures the object and returns its desired size.
+        /// </summary>
+        /// <param name="constraints">The width and height that the object is not allowed to exceed.</param>
+        /// <returns>The desired size as a <see cref="Size"/> instance.</returns>
+        public Size Measure(Size constraints)
+        {
+            return new Size(constraints.Width, Height);
+        }
+
+        /// <summary>
+        /// Provides the behavior for the Arrange pass of layout. Classes can override this method to define their own Arrange pass behavior.
+        /// </summary>
+        /// <param name="finalSize">The final area within the parent that this object should use to arrange itself and its children.</param>
+        protected override global::Windows.Foundation.Size ArrangeOverride(global::Windows.Foundation.Size finalSize)
+        {
+            ArrangeRequest(false, null);
+            finalSize = Frame.Size.GetSize();
+            base.ArrangeOverride(finalSize);
+            return finalSize;
+        }
+
+        /// <summary>
+        /// Provides the behavior for the Measure pass of the layout cycle. Classes can
+        /// override this method to define their own Measure pass behavior.
+        /// </summary>
+        /// <param name="availableSize">The available size that this object can give to child objects. 
+        /// Infinity can be specified as a value to indicate that the object will size to whatever content is available.</param>
+        protected override global::Windows.Foundation.Size MeasureOverride(global::Windows.Foundation.Size availableSize)
+        {
+            var desiredSize = MeasureRequest(false, null).GetSize();
+            base.MeasureOverride(desiredSize);
+            return desiredSize;
         }
 
         private void OnPropertyChanged(PropertyDescriptor pd)
