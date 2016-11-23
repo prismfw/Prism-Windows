6c01a6139773ecec1cc83c8213b0974e5187b808
jonathan-git <jonathan-git@users.noreply.github.com>
2016-11-22 22:43:57 -0600

    Implemented new LoadIndicator APIs.

---
+++ UI/Controls/LoadIndicator.cs
@@ -0,0 +1,299 @@
+/*
+Copyright (C) 2016  Prism Framework Team
+
+This file is part of the Prism Framework.
+
+The Prism Framework is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+The Prism Framework is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+using System;
+using System.Linq;
+using Prism.Native;
+using Prism.UI;
+using Prism.UI.Media;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace Prism.Windows.UI.Controls
+{
+    /// <summary>
+    /// Represents a Windows implementation for an <see cref="INativeLoadIndicator"/>.
+    /// </summary>
+    [Register(typeof(INativeLoadIndicator))]
+    public class LoadIndicator : Grid, INativeLoadIndicator
+    {
+        /// <summary>
+        /// Occurs when this instance has been attached to the visual tree and is ready to be rendered.
+        /// </summary>
+        public new event EventHandler Loaded;
+
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event EventHandler<FrameworkPropertyChangedEventArgs> PropertyChanged;
+
+        /// <summary>
+        /// Occurs when this instance has been detached from the visual tree.
+        /// </summary>
+        public new event EventHandler Unloaded;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether animations are enabled for this instance.
+        /// </summary>
+        public bool AreAnimationsEnabled
+        {
+            get { return areAnimationsEnabled; }
+            set
+            {
+                if (value != areAnimationsEnabled)
+                {
+                    areAnimationsEnabled = value;
+                    if (areAnimationsEnabled)
+                    {
+                        Transitions = transitions;
+                        popup.Transitions = elementTransitions;
+                        popup.ChildTransitions = elementChildTransitions;
+                    }
+                    else
+                    {
+                        transitions = Transitions;
+                        elementTransitions = popup.Transitions;
+                        elementChildTransitions = popup.ChildTransitions;
+
+                        Transitions = null;
+                        popup.Transitions = null;
+                        popup.ChildTransitions = null;
+                    }
+
+                    OnPropertyChanged(Visual.AreAnimationsEnabledProperty);
+                }
+            }
+        }
+        private bool areAnimationsEnabled = true;
+        private TransitionCollection transitions, elementTransitions, elementChildTransitions;
+
+        /// <summary>
+        /// Gets or sets the method to invoke when this instance requests an arrangement of its children.
+        /// </summary>
+        public ArrangeRequestHandler ArrangeRequest { get; set; }
+
+        /// <summary>
+        /// Gets or sets the background of the indicator.
+        /// </summary>
+        public new Brush Background
+        {
+            get { return background; }
+            set
+            {
+                if (value != background)
+                {
+                    background = value;
+                    base.Background = value.GetBrush() ?? new global::Windows.UI.Xaml.Media.SolidColorBrush(global::Windows.UI.Color.FromArgb(60, 0, 0, 0));
+                    OnPropertyChanged(Prism.UI.Controls.LoadIndicator.BackgroundProperty);
+                }
+            }
+        }
+        private Brush background;
+
+        /// <summary>
+        /// Gets or sets the panel that contains the content for the indicator.
+        /// </summary>
+        public object Content
+        {
+            get { return Children.FirstOrDefault(); }
+            set
+            {
+                Children.Clear();
+
+                var element = value as UIElement;
+                if (element != null)
+                {
+                    Children.Add(element);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a <see cref="Rectangle"/> that represents the size and position of the object relative to its parent container.
+        /// </summary>
+        public Rectangle Frame
+        {
+            get { return frame; }
+            set
+            {
+                frame = value;
+
+                popup.HorizontalOffset = frame.X;
+                popup.VerticalOffset = frame.Y;
+                popup.Width = base.Width = value.Width;
+                popup.Height = base.Height = value.Height;
+            }
+        }
+        private Rectangle frame = new Rectangle();
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance can be considered a valid result for hit testing.
+        /// </summary>
+        public new bool IsHitTestVisible
+        {
+            get { return base.IsHitTestVisible; }
+            set
+            {
+                if (value != base.IsHitTestVisible)
+                {
+                    base.IsHitTestVisible = value;
+                    OnPropertyChanged(Visual.IsHitTestVisibleProperty);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance has been loaded and is ready for rendering.
+        /// </summary>
+        public bool IsLoaded { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the method to invoke when this instance requests a measurement of itself and its children.
+        /// </summary>
+        public MeasureRequestHandler MeasureRequest { get; set; }
+
+        /// <summary>
+        /// Gets or sets transformation information that affects the rendering position of this instance.
+        /// </summary>
+        public new INativeTransform RenderTransform
+        {
+            get { return renderTransform; }
+            set
+            {
+                if (value != renderTransform)
+                {
+                    renderTransform = value;
+                    base.RenderTransform = renderTransform as Media.Transform ?? renderTransform as global::Windows.UI.Xaml.Media.Transform;
+                    OnPropertyChanged(Visual.RenderTransformProperty);
+                }
+            }
+        }
+        private INativeTransform renderTransform;
+
+        /// <summary>
+        /// Gets or sets the visual theme that should be used by this instance.
+        /// </summary>
+        public new Theme RequestedTheme
+        {
+            get { return base.RequestedTheme.GetTheme(); }
+            set { base.RequestedTheme = value.GetElementTheme(); }
+        }
+
+        private readonly global::Windows.UI.Xaml.Controls.Primitives.Popup popup;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoadIndicator"/> class.
+        /// </summary>
+        public LoadIndicator()
+        {
+            base.Loaded += (o, e) =>
+            {
+                if (!IsLoaded)
+                {
+                    IsLoaded = true;
+                    OnPropertyChanged(Visual.IsLoadedProperty);
+                    Loaded(this, EventArgs.Empty);
+                }
+            };
+
+            base.Unloaded += (o, e) =>
+            {
+                if (IsLoaded)
+                {
+                    IsLoaded = false;
+                    OnPropertyChanged(Visual.IsLoadedProperty);
+                    Unloaded(this, EventArgs.Empty);
+                }
+            };
+
+            popup = new global::Windows.UI.Xaml.Controls.Primitives.Popup()
+            {
+                Child = this,
+                HorizontalAlignment = global::Windows.UI.Xaml.HorizontalAlignment.Center,
+                VerticalAlignment = global::Windows.UI.Xaml.VerticalAlignment.Center
+            };
+
+            base.Background = new global::Windows.UI.Xaml.Media.SolidColorBrush(global::Windows.UI.Color.FromArgb(60, 0, 0, 0));
+        }
+
+        /// <summary>
+        /// Removes the indicator from view.
+        /// </summary>
+        public void Hide()
+        {
+            popup.IsOpen = false;
+        }
+
+        /// <summary>
+        /// Measures the object and returns its desired size.
+        /// </summary>
+        /// <param name="constraints">The width and height that the object is not allowed to exceed.</param>
+        /// <returns>The desired size as a <see cref="Size"/> instance.</returns>
+        public Size Measure(Size constraints)
+        {
+            return new Size(CoreApplication.MainView.CoreWindow.Bounds.Width, CoreApplication.MainView.CoreWindow.Bounds.Height);
+        }
+
+        /// <summary>
+        /// Displays the indicator.
+        /// </summary>
+        public void Show()
+        {
+            popup.IsOpen = true;
+        }
+
+        /// <summary>
+        /// Provides the behavior for the Arrange pass of layout. Classes can override this method to define their own Arrange pass behavior.
+        /// </summary>
+        /// <param name="finalSize">The final area within the parent that this object should use to arrange itself and its children.</param>
+        protected override global::Windows.Foundation.Size ArrangeOverride(global::Windows.Foundation.Size finalSize)
+        {
+            ArrangeRequest(false, null);
+            finalSize = Frame.Size.GetSize();
+            base.ArrangeOverride(finalSize);
+            return finalSize;
+        }
+
+        /// <summary>
+        /// Provides the behavior for the Measure pass of the layout cycle. Classes can
+        /// override this method to define their own Measure pass behavior.
+        /// </summary>
+        /// <param name="availableSize">The available size that this object can give to child objects. 
+        /// Infinity can be specified as a value to indicate that the object will size to whatever content is available.</param>
+        protected override global::Windows.Foundation.Size MeasureOverride(global::Windows.Foundation.Size availableSize)
+        {
+            var desiredSize = MeasureRequest(false, null).GetSize();
+            base.MeasureOverride(desiredSize);
+            return desiredSize;
+        }
+
+        /// <summary>
+        /// Called when a property value is changed.
+        /// </summary>
+        /// <param name="pd">A property descriptor describing the property whose value has been changed.</param>
+        protected virtual void OnPropertyChanged(PropertyDescriptor pd)
+        {
+            PropertyChanged(this, new FrameworkPropertyChangedEventArgs(pd));
+        }
+    }
+}
