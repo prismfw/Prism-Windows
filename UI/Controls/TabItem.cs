e3a8c0e69d284bfb72c52f6313d65f8b8b1b5687
jonathan-git <jonathan-git@users.noreply.github.com>
2016-12-06 22:42:16 -0600

    Implemented new TabItem APIs.

--- UI/Controls/TabItem.cs
+++ UI/Controls/TabItem.cs
@@ -25,6 +25,7 @@ using Prism.UI.Media;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media.Animation;
 
 namespace Prism.Windows.UI.Controls
 {
@@ -35,9 +36,56 @@ namespace Prism.Windows.UI.Controls
     public class TabItem : StackPanel, INativeTabItem
     {
         /// <summary>
+        /// Occurs when this instance has been attached to the visual tree and is ready to be rendered.
+        /// </summary>
+        public new event EventHandler Loaded;
+
+        /// <summary>
         /// Occurs when the value of a property is changed.
         /// </summary>
         public event EventHandler<FrameworkPropertyChangedEventArgs> PropertyChanged;
+
+        /// <summary>
+        /// Occurs when this instance has been detached from the visual tree.
+        /// </summary>
+        public new event EventHandler Unloaded;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether animations are enabled for this instance.
+        /// </summary>
+        public bool AreAnimationsEnabled
+        {
+            get { return areAnimationsEnabled; }
+            set
+            {
+                if (value != areAnimationsEnabled)
+                {
+                    areAnimationsEnabled = value;
+                    if (areAnimationsEnabled)
+                    {
+                        Transitions = transitions;
+                        ChildrenTransitions = childrenTransitions;
+                    }
+                    else
+                    {
+                        transitions = Transitions;
+                        childrenTransitions = ChildrenTransitions;
+
+                        Transitions = null;
+                        ChildrenTransitions = null;
+                    }
+
+                    OnPropertyChanged(Prism.UI.Visual.AreAnimationsEnabledProperty);
+                }
+            }
+        }
+        private bool areAnimationsEnabled = true;
+        private TransitionCollection transitions, childrenTransitions;
+
+        /// <summary>
+        /// Gets or sets the method to invoke when this instance requests an arrangement of its children.
+        /// </summary>
+        public ArrangeRequestHandler ArrangeRequest { get; set; }
 
         /// <summary>
         /// Gets or sets the object that acts as the content of the item.
@@ -145,6 +193,22 @@ namespace Prism.Windows.UI.Controls
         private Brush foreground;
 
         /// <summary>
+        /// Gets or sets a <see cref="Rectangle"/> that represents the size and position of the object relative to its parent container.
+        /// </summary>
+        public Rectangle Frame
+        {
+            get { return frame; }
+            set
+            {
+                frame = value;
+
+                Width = value.Width;
+                Height = value.Height;
+            }
+        }
+        private Rectangle frame = new Rectangle();
+
+        /// <summary>
         /// Gets or sets an <see cref="INativeImageSource"/> for an image to display with the item.
         /// </summary>
         public INativeImageSource ImageSource
@@ -161,6 +225,66 @@ namespace Prism.Windows.UI.Controls
             }
         }
         private INativeImageSource imageSource;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance can be considered a valid result for hit testing.
+        /// </summary>
+        public new bool IsHitTestVisible
+        {
+            get { return base.IsHitTestVisible; }
+            set
+            {
+                if (value != base.IsHitTestVisible)
+                {
+                    base.IsHitTestVisible = value;
+
+                    var presenter = this.GetParent<ListViewItemPresenter>();
+                    if (presenter != null)
+                    {
+                        presenter.IsHitTestVisible = base.IsHitTestVisible;
+                    }
+
+                    OnPropertyChanged(Prism.UI.Visual.IsHitTestVisibleProperty);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance has been loaded and is ready for rendering.
+        /// </summary>
+        public bool IsLoaded { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the method to invoke when this instance requests a measurement of itself and its children.
+        /// </summary>
+        public MeasureRequestHandler MeasureRequest { get; set; }
+
+        /// <summary>
+        /// Gets or sets transformation information that affects the rendering position of this instance.
+        /// </summary>
+        public new INativeTransform RenderTransform
+        {
+            get { return renderTransform; }
+            set
+            {
+                if (value != renderTransform)
+                {
+                    renderTransform = value;
+                    base.RenderTransform = renderTransform as Media.Transform ?? renderTransform as global::Windows.UI.Xaml.Media.Transform;
+                    OnPropertyChanged(Prism.UI.Visual.RenderTransformProperty);
+                }
+            }
+        }
+        private INativeTransform renderTransform;
+
+        /// <summary>
+        /// Gets or sets the visual theme that should be used by this instance.
+        /// </summary>
+        public new Prism.UI.Theme RequestedTheme
+        {
+            get { return base.RequestedTheme.GetTheme(); }
+            set { base.RequestedTheme = value.GetElementTheme(); }
+        }
 
         /// <summary>
         /// Gets or sets the title for the item.
@@ -214,16 +338,70 @@ namespace Prism.Windows.UI.Controls
             HorizontalAlignment = HorizontalAlignment.Stretch;
             MinHeight = 48;
             Orientation = Orientation.Horizontal;
+            RenderTransformOrigin = new global::Windows.Foundation.Point(0.5, 0.5);
 
-            Loaded += (o, e) =>
+            base.Loaded += (o, e) =>
             {
                 var presenter = this.GetParent<ListViewItemPresenter>();
                 if (presenter != null)
                 {
+                    presenter.IsHitTestVisible = base.IsHitTestVisible;
                     presenter.SelectedBackground = presenter.SelectedPointerOverBackground =
                         presenter.GetParent<INativeTabView>()?.Foreground.GetBrush() ?? SelectedBackgroundDefault;
                 }
+
+                if (!IsLoaded)
+                {
+                    IsLoaded = true;
+                    OnPropertyChanged(Prism.UI.Visual.IsLoadedProperty);
+                    Loaded(this, EventArgs.Empty);
+                }
             };
+
+            base.Unloaded += (o, e) =>
+            {
+                if (IsLoaded)
+                {
+                    IsLoaded = false;
+                    OnPropertyChanged(Prism.UI.Visual.IsLoadedProperty);
+                    Unloaded(this, EventArgs.Empty);
+                }
+            };
+        }
+
+        /// <summary>
+        /// Measures the object and returns its desired size.
+        /// </summary>
+        /// <param name="constraints">The width and height that the object is not allowed to exceed.</param>
+        /// <returns>The desired size as a <see cref="Size"/> instance.</returns>
+        public Size Measure(Size constraints)
+        {
+            return base.MeasureOverride(constraints.GetSize()).GetSize();
+        }
+
+        /// <summary>
+        /// Provides the behavior for the Arrange pass of layout. Classes can override this method to define their own Arrange pass behavior.
+        /// </summary>
+        /// <param name="finalSize">The final area within the parent that this object should use to arrange itself and its children.</param>
+        protected override global::Windows.Foundation.Size ArrangeOverride(global::Windows.Foundation.Size finalSize)
+        {
+            ArrangeRequest(false, null);
+            finalSize = Frame.Size.GetSize();
+            base.ArrangeOverride(finalSize);
+            return finalSize;
+        }
+
+        /// <summary>
+        /// Provides the behavior for the Measure pass of the layout cycle. Classes can
+        /// override this method to define their own Measure pass behavior.
+        /// </summary>
+        /// <param name="availableSize">The available size that this object can give to child objects. 
+        /// Infinity can be specified as a value to indicate that the object will size to whatever content is available.</param>
+        protected override global::Windows.Foundation.Size MeasureOverride(global::Windows.Foundation.Size availableSize)
+        {
+            var desiredSize = MeasureRequest(false, null).GetSize();
+            base.MeasureOverride(desiredSize);
+            return desiredSize;
         }
 
         /// <summary>
